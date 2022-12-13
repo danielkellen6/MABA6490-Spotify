1957c8f2cc52e895de6ce9cb21af432f01002189
danielkellen6 <danielkellen6@gmail.com>
2022-12-13 15:33:32 -0600

    Interactive table

--- rec_app_interactiveTable.py
+++ rec_app_interactiveTable.py
@@ -14,12 +14,15 @@ import pickle
 from googlesearch import search
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
 
 header = st.container()
 user_input = st.container()
 video = st.container()
 data = st.container()
 cluster = st.container()
+
 
 #DataFrame
 with open("full_dataset_df.pkl", "rb") as file:
@@ -54,15 +57,13 @@ with user_input:
     energy_dict = dict(zip(nrg1,score))
 
 
-    col1, col2 = st.columns(2)
+    list_length = st.slider("How Many Songs do you want in your Playlist?", min_value = 2, max_value = 25, value = 15, step = 1)
 
-    list_length = col1.slider("How Many Songs do you want in your Playlist?", min_value = 2, max_value = 25, value = 15, step = 1)
-
-    pop = col1.select_slider("Hipster or Mainstream?",options = pop1,value = pop1[2])
-    dance = col1.select_slider('How bad do you want to dance?', options = dance1, value = dance1[2])
-    nrg = col1.select_slider("Energy", options = nrg1, value = nrg1[2])
-    val= col1.select_slider("Mood",options = val1, value = val1[2])
-    temp = col1.select_slider("Tempo, Tempo, Tempo", options = temp1, value = temp1[2])
+    pop = st.select_slider("Hipster or Mainstream?",options = pop1,value = pop1[2])
+    dance = st.select_slider('How bad do you want to dance?', options = dance1, value = dance1[2])
+    nrg = st.select_slider("Energy", options = nrg1, value = nrg1[2])
+    val= st.select_slider("Mood",options = val1, value = val1[2])
+    temp = st.select_slider("Tempo, Tempo, Tempo", options = temp1, value = temp1[2])
 
     danceability = dance_dict.get(dance)
     popularity = pop_dict.get(pop)
@@ -73,7 +74,7 @@ with user_input:
     song_input = [popularity, danceability, energy, valence, tempo]
 
     playlist_length = list_length
-    
+
     neigh = NearestNeighbors(n_neighbors=playlist_length)
     neigh.fit(data[["popularity", "danceability", "energy", "valence", "tempo"]])
     NearestNeighbors(algorithm='auto', leaf_size=30)
@@ -94,21 +95,68 @@ with user_input:
 # Inject CSS with Markdown
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
-    playlist = pd.DataFrame(data[["song name", "artist"]].iloc[song])
-    col2.table(playlist)
+    # playlist = pd.DataFrame(data[["song name", "artist"]].iloc[song])
+    # st.table(playlist)
 
-col3, col4 = st.columns(2)
+    playlist2 = pd.DataFrame(data[["song name", "artist"]].iloc[song])
+
+    def aggrid_interactive_table(df: pd.DataFrame):
+        """Creates an st-aggrid interactive table based on a dataframe.
+
+        Args:
+            df (pd.DataFrame]): Source dataframe
+
+        Returns:
+            dict: The selected row
+        """
+        options = GridOptionsBuilder.from_dataframe(
+            df, enableRowGroup=True, enableValue=True, enablePivot=True,
+        )
+
+        options.configure_side_bar()
+
+        options.configure_selection("single")
+        selection = AgGrid(
+            df,
+            enable_enterprise_modules=True,
+            gridOptions=options.build(),
+            update_mode=GridUpdateMode.MODEL_CHANGED,
+            allow_unsafe_jscode=True,
+        )
+
+        return selection
+
+
+    playlist2 = pd.DataFrame(data[["song name", "artist"]].iloc[song])
+    st.write("Select a Song from the playlist to listen via YouTube")
+    selection = aggrid_interactive_table(df=playlist2)
+
+    if selection["selected_rows"] == []:
+        st.write("Select Song")
+        # st.json(selection["selected_rows"])
+    else:
+        sn = selection["selected_rows"][0]["song name"]
+        art = selection["selected_rows"][0]["artist"]
+        st.write(selection["selected_rows"][0]["song name"])
+        term = sn + " " + art + " " + "youtube"
+        for url2 in search(term, stop=1):
+            url_link2 = url2
+        st.video(url_link2)
+
 with cluster:
+    col1, col2 = st.columns(2)
     st.header("The Song EcoSystem")
     user_cluster = cluster_model.predict(np.array(song_input).reshape(1,-1))
-    col3.plotly_chart(cluster_image, user_container_width = True)
-
-    col4.write("You may also like songs from cluster {}".format(user_cluster[0]))
+    col1.write("You may also like songs from cluster {}".format(user_cluster[0]))
+    col1.plotly_chart(cluster_image, user_container_width = True)
     user_cluster_df = data[data['cluster'] == user_cluster[0]]
-    col4.table(user_cluster_df[['song name', 'artist']][:50])
+    col2.table(user_cluster_df[['song name', 'artist']][:15])
 
-with video:
-    search_term = playlist['song name'].values[0] +" "+ playlist["artist"].values[0] + " " + "youtube"
-    for url in search(search_term, stop=1):
-        url_link = url
-    st.video(url_link)
+
+
+
+# with video:
+#     search_term = playlist['song name'].values[0] +" "+ playlist["artist"].values[0] + " " + "youtube"
+#     for url in search(search_term, stop=1):
+#         url_link = url
+#     st.video(url_link)
